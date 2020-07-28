812c0b890c18124064ed53876db1d9db01a579b4
Nia Smith <niasmith94@outlook.com>
2020-07-28 15:11:13 -0400

    passowrd kinda working

--- userInput.js
+++ userInput.js
@@ -1,18 +1,34 @@
 const fs = require("fs");
 const inquirer = require ("inquirer");
 const greeting = require ("./greeting");
-const PasswordPrompt = require("inquirer/lib/prompts/password");
+// const PasswordPrompt = require("inquirer/lib/prompts/password");
  // // log greeting on userInput page
 console.log(greeting);
 inquirer
 // Prompt the new user to create their new user name
-  .prompt([
+//   .prompt([
+//     {
+//     name: 'NiaSmith',
+//     message: 'What is your username?',
+//     },
+//   ])
+//   Answer for the user name the new user just entered
+//   .then(answers => {
+//     console.info('Answer:', answers.NiaSmith);
+
+    
+//   });
+
+// const inquirer = require('inquirer');
+
+inquirer
     {
-    name: 'NiaSmith',
-    message: 'What is your username?',
+      type: 'password',
+      name: 'secret',
+      message: 'Enter password here',
     },
   ])
-//   Answer for the user name the new user just entered
   .then(answers => {
-    console.info('Answer:', answers.NiaSmith);
+    // Logging out the secret defeats the purpose though ;)
+    console.info('Answer:', answers.secret);
   });
