9c753bb94c41848d7b2969cdd7cf4390fdf8b124
Nia Smith <niasmith94@outlook.com>
2020-07-28 17:22:24 -0400

    message

--- userInput.js
+++ userInput.js
@@ -1,33 +1,40 @@
+// Node.js file system and HTTP modules required to work
 const fs = require("fs");
+const http = require("http");
+// npm inquirer file 
 const inquirer = require ("inquirer");
+// connector to allow userInput to call greeting
 const greeting = require ("./greeting");
+
 
 console.log(greeting);
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "What is your user name?",
-      name: "username"
-    },
-    {
-      type: "password",
-      message: "What is your password?",
-      name: "password"
-    },
-    {
-      type: "password",
-      message: "Re-enter password to confirm:",
-      name: "confirm"
-    }
-  ])
-  .then(function(response) {
+    inquirer
+    .prompt([
+        {
+        type: "input",
+        message: "What is your user name?",
+        name: "username"
+        },
 
-    if (response.confirm === response.password) {
-      console.log("Welcome!");
-    }
-    else {
-      console.log("Oopsy, try again!");
-    }
-  });
+        {
+        type: "password",
+        message: "What is your password?",
+        name: "password"
+        },
+        {
+        type: "password",
+        message: "Re-enter password to confirm",
+        name: "confirm"
+        },
+        
+    ])
+    .then(function(response) {
+
+        if (response.confirm === response.password) {
+        console.log("Welcome!");
+        }
+        else {
+        console.log("Oopsy, try again!");
+        }
+    });
