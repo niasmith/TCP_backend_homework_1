c99eab3d783b3d6d070538d0f1073b37d7db301b
Nia Smith <niasmith94@outlook.com>
2020-07-27 21:16:21 -0400

    added work

---
+++ greeting.js
@@ -0,0 +1,3 @@
+var greeting = "hello new user";
+// module.export is used like the reserve of requirer. It sends the greeting message to userInput.js
+module.exports = greeting;
---
+++ userInput.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const inquirer = require ("inquirer");
+// log greeting on userInput page
+const greeting = require ("./greeting");
+console.log(greeting);
+function userQuestions(){
+    
+};
