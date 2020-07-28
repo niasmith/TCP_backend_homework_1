84239319f014f5252d6c39a1fd3177697362988e
Nia Smith <niasmith94@outlook.com>
2020-07-28 14:34:20 -0400

    user prompt working

--- userInput.js
+++ userInput.js
@@ -1,8 +1,18 @@
 const fs = require("fs");
 const inquirer = require ("inquirer");
-// log greeting on userInput page
 const greeting = require ("./greeting");
+const PasswordPrompt = require("inquirer/lib/prompts/password");
+ // // log greeting on userInput page
 console.log(greeting);
-function userQuestions(){
-    
-};
+inquirer
+// Prompt the new user to create their new user name
+  .prompt([
+    {
+    name: 'NiaSmith',
+    message: 'What is your username?',
+    },
+  ])
+//   Answer for the user name the new user just entered
+  .then(answers => {
+    console.info('Answer:', answers.NiaSmith);
+  });
